19540b699ba636f3853cad5af5727ce801812ec0
Hedges07 <Hedges07@gmail.com>
2023-10-19 16:50:34 -0400

    Connecting to new DB location

--- BookStoreLIB/Properties/Settings.Designer.cs
+++ BookStoreLIB/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace BookStoreLIB.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Andrew\\source\\repos\\" +
-            "Bookstoreshit\\BookStoreGUI\\asBookstoreDB.mdf;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Admin\\Documents\\GitH" +
+            "ub\\Group-Project-4220\\BookStoreGUI\\asBookstoreDB.mdf;Integrated Security=True")]
         public string asConnectionString {
             get {
                 return ((string)(this["asConnectionString"]));
