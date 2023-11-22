fdea63b012b195bbaa03001623210aeb1f639e83
Ryohei Takamori <148498367+Udonboii30@users.noreply.github.com>
2023-11-22 17:26:38 -0500

    New table in the DB
    added review table to the db

---
+++ SQL/Reviews.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Reviews] (
+    [ReviewID]   INT            IDENTITY(1,1) NOT NULL,
+    [Rating]     INT            NOT NULL,
+    [UserID]     INT            NOT NULL,
+    [ISBN]       CHAR (10)      NOT NULL,
+    CONSTRAINT [PK_Reviews] PRIMARY KEY CLUSTERED ([ReviewID] ASC),
+    CONSTRAINT [FK_Reviews_User] FOREIGN KEY ([UserID]) REFERENCES [dbo].[UserData] ([UserID]),
+    CONSTRAINT [FK_Reviews_BookData] FOREIGN KEY ([ISBN]) REFERENCES [dbo].[BookData] ([ISBN])
+);
